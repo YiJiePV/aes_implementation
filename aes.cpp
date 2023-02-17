dcb87b2b22d5c0aee82e33cfca00a59fbee6c5ea
Dandebayo <olasubomidadebayo@gmail.com>
2023-02-16 22:13:28 -0800

    changes to encryption function to print the encrypted message and made the code read the file

--- aes.cpp
+++ aes.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <cstdint>
 #include <vector>
+#include <fstream>
 
 using std::string;
 using std::cout;
@@ -28,41 +29,41 @@ const uint8_t sbox[256] = {
   0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB,
 };
 //Function to substitute the bytes
-void SubBytes(uint8_t state[4][4]) {
+void SubBytes(uint8_t matrix[4][4]) {
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
-      state[i][j] = sbox[state[i][j]];
+      matrix[i][j] = sbox[matrix[i][j]];
     }
   }
 }
 
 /// Function to Shift the rows
-void ShiftRows(uint8_t state[4][4]) {
+void ShiftRows(uint8_t matrix[4][4]) {
   uint8_t temp[4];
   // Shift first row by 0
   // Shift second row by 1
-  temp[0] = state[1][0];
-  temp[1] = state[1][1];
-  temp[2] = state[1][2];
-  temp[3] = state[1][3];
+  temp[0] = matrix[1][0];
+  temp[1] = matrix[1][1];
+  temp[2] = matrix[1][2];
+  temp[3] = matrix[1][3];
   for (int i = 0; i < 4; i++) {
-    state[1][i] = temp[(i + 1) % 4];
+    matrix[1][i] = temp[(i + 1) % 4];
   }
   // Shift third row by 2
-  temp[0] = state[2][0];
-  temp[1] = state[2][1];
-  temp[2] = state[2][2];
-  temp[3] = state[2][3];
+  temp[0] = matrix[2][0];
+  temp[1] = matrix[2][1];
+  temp[2] = matrix[2][2];
+  temp[3] = matrix[2][3];
   for (int i = 0; i < 4; i++) {
-    state[2][i] = temp[(i + 2) % 4];
+    matrix[2][i] = temp[(i + 2) % 4];
   }
   // Shift fourth row by 3
-  temp[0] = state[3][0];
-  temp[1] = state[3][1];
-  temp[2] = state[3][2];
-  temp[3] = state[3][3];
+  temp[0] = matrix[3][0];
+  temp[1] = matrix[3][1];
+  temp[2] = matrix[3][2];
+  temp[3] = matrix[3][3];
   for (int i = 0; i < 4; i++) {
-    state[3][i] = temp[(i + 3) % 4];
+    matrix[3][i] = temp[(i + 3) % 4];
   }
 }
 
@@ -196,7 +197,15 @@ string Encrypt(const string& input){
   cout << hex << int(result[1][0]) << ", " << int(result[1][1]) << ", "  << int(result[1][2]) << ", "  << int(result[1][3]) << endl;
   cout << hex << int(result[2][0]) << ", " << int(result[2][1]) << ", "  << int(result[2][2]) << ", "  << int(result[2][3]) << endl;
   cout << hex << int(result[3][0]) << ", " << int(result[3][1]) << ", "  << int(result[3][2]) << ", "  << int(result[3][3]) << endl;
-  return ""; //ciphertext
+
+for (int i = 0; i <4; i++){
+	for(int j = 0; j < 4; j++){
+		cout << grid[i][j];
+	}
+  }
+cout <<endl;
+
+  return text; //ciphertext
 }
 //AES decryption (cipher: 128 bit/16 bytes of ciphertext):
 string Decrypt(string cipher){
@@ -223,7 +232,17 @@ string Decrypt(string cipher){
 }
 //command line user interface
 int main() {
+string text;
   //get file input
+std::fstream myfile;
+ myfile.open("hellobaby.txt");
+ if (myfile.is_open()){
+ 
+ while(getline(myfile, text)){
+     
+ }
+ myfile.close();
+   }
 
   // uint8_t grid[4][4] = {{0xd4, 0xd4, 0xd4, 0xd4}, 
   //                     {0xbf, 0xbf, 0xbf, 0xbf}, 
